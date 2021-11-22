d460ba923d5231ef22ee1f4d925d948480b76aae
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-22 09:41:23 -0500

    Short way to declare props

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,24 +1,20 @@
 (() => {
     
     class Avenger {
-        // acceso a la propiedad dentro SOLO de la clase.
-        private name: string;
-        public team: string;
-        public realName?: string;
         static avgAge: number = 35;
         
-        constructor (name: string, team: string, realName?:string) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        };
+        constructor (
+            private name: string,
+            public team: string,
+            public realName?: string,
+        ) {};
     };
-
     
-    const atman:Avenger = new Avenger('Camilo', 'runner');
+    const atman:Avenger = new Avenger('Camilo', 'runner', 'juank');
     console.log(atman);
 
     // acceder a la propiedad estatica
     // console.log(Avenger.avgAge); // --> 35
 
+    console.log(Avenger.avgAge) // 55
 })()
