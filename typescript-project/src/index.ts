583d3af324030612086e8cec6756925c8fd8da5b
enigma <camilo.murcia.pimentel@gmail.com>
2021-12-14 16:23:44 -0500

    Add generic functions

--- typescript-project/src/index.ts
+++ typescript-project/src/index.ts
@@ -1,13 +1,20 @@
-import { Hero } from "./classes/Hero";
-// import { Hero as SuperHero, Hero2  } from "./classes/Hero";
-// import * as HeroClasses from "./classes/Hero";
+import { printObject, genericFunction, genericFunctionArrow } from "./generics/generics";
 
-// we can put whatever name at import our file
-// import powercito from './data/powers';
+// printObject(123);
+// printObject(new Date());
+// printObject({a:1, b:2, c:3});
+// printObject([1,2,3,4,5,6,7,8,9,10]);
+// printObject('Hello world');
 
-// const Hero = 123;
 
-// const ironman = new SuperHero('Ironman', 5000, 45);
-const ironman = new Hero('Ironman', 5000, 45);
-console.log(ironman);
-console.log(ironman.powerId);
+// como le podemos decir a typescript que me muestre los metodos que
+// correspondan con el tipo de dato que le ingresamos?
+console.log(genericFunction(3.145678).toFixed(2));      
+console.log(genericFunction('Hola mundo').toUpperCase());      
+console.log(genericFunction(new Date()).getHours());      
+
+console.log(genericFunctionArrow(15.645678).toFixed(2));      
+console.log(genericFunctionArrow('JUAN CAMILO').toLowerCase());      
+console.log(genericFunctionArrow(new Date(1)).getHours());      
+
+
