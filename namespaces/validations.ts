c673dcaf1e71cf653aaac28d5626e873ef04c8ff
enigma <camilo.murcia.pimentel@gmail.com>
2021-12-14 11:21:39 -0500

    Add functions into namespaces

---
+++ namespaces/validations.ts
@@ -0,0 +1,15 @@
+// agrupador de clases o funciones
+namespace Validations {
+
+    export const validateText = (text:string): boolean => {
+        return (text.length > 3) ? true : false;
+    }
+
+    export const validateDate = (myDate: Date): boolean => {
+        return (isNaN(myDate.valueOf()))
+        ? false
+        : true;
+    }
+}
+
+console.log(Validations.validateText('Camilo'));
