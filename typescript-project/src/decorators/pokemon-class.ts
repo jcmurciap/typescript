eb9e608515351a79f8049f23cd7b8e510825a68d
enigma <camilo.murcia.pimentel@gmail.com>
2021-12-15 10:53:21 -0500

    Add factories decorator

---
+++ typescript-project/src/decorators/pokemon-class.ts
@@ -0,0 +1,24 @@
+function printToConsole(constructor: Function){
+    console.log(constructor)
+}
+
+const printToConsoleConditional = (print: boolean = false):Function => {
+    if(print){
+
+        return printToConsole; 
+    }else {
+        return () => {}
+    }
+}
+
+@printToConsoleConditional(false)
+export class Pokemon {
+
+    public publicApi: string = "https://pokeapi.co";
+    constructor (
+        public name: string,
+    ){}
+}
+
+
+
